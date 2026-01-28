926287af15698b53262c08643e3169a3b4b86b02
ShaunMcelhinney <shaun@vegascity.org>
2026-01-28 16:33:41 +0000

    Progress added into the UI  and hooked up to the api, resorting of component folder system for better readability

--- src/components/ItemsAndEquipmentComponents/ItemModal.tsx
+++ src/components/ItemsAndEquipmentComponents/ItemModal.tsx
@@ -8,47 +8,51 @@ type Props = {
     updateItems: () => void;
 }
 
-export default function ItemModal({ItemType, setShowItemModal, updateItems}: Props){
+export default function ItemModal({ ItemType, setShowItemModal, updateItems }: Props) {
 
     const { character } = useCharacterStore();
     const [name, setName] = useState<string>('');
     const [effect, setEffect] = useState<string>('');
 
-    function handleSubmit(e: FormEvent<HTMLFormElement>, id: number){
+    function handleSubmit(e: FormEvent<HTMLFormElement>, id: number) {
         e.preventDefault();
         const item: ItemDto = {
             name: name,
             effect: effect,
             characterId: id
         };
-        switch(ItemType){
+        switch (ItemType) {
             case 'equipment':
                 addEquipment(item).then(res => {
                     console.log(res)
                     updateItems();
+                }).catch((error: Error) => {
+                    console.log(error)
                 });
                 break;
             case 'potion':
                 addPotion(item).then(res => {
                     console.log(res)
                     updateItems();
-                });;
+                }).catch((error: Error) => {
+                    console.log(error)
+                });
                 break;
         }
-       
+
         setShowItemModal(false);
     }
 
-    return(
-         <div className='modal-background'>
+    return (
+        <div className='modal-background'>
             <div className='modal-container'>
                 <h3>Create Item</h3>
-                <form onSubmit={(e) => handleSubmit(e, character.id)} style={{display: 'flex', flexDirection: 'column'}}>
-                <label htmlFor='item-name'>Item Name: </label>
-                <input id='item-name' type='text' value={name} onChange={(e) => setName(e.target.value)}/>
-                <label htmlFor='item-effect'>Item Effect: </label>
-                <input id='item-effect' type='text' value={effect} onChange={(e) => setEffect(e.target.value)}/>
-                <button type='submit'>Submit</button>
+                <form onSubmit={(e) => handleSubmit(e, character.id)} style={{ display: 'flex', flexDirection: 'column' }}>
+                    <label htmlFor='item-name'>Item Name: </label>
+                    <input id='item-name' type='text' value={name} onChange={(e) => setName(e.target.value)} />
+                    <label htmlFor='item-effect'>Item Effect: </label>
+                    <input id='item-effect' type='text' value={effect} onChange={(e) => setEffect(e.target.value)} />
+                    <button type='submit'>Submit</button>
                 </form>
                 <button onClick={() => setShowItemModal(false)}>Close</button>
             </div>
--- src/store.ts
+++ src/store.ts
@@ -27,22 +27,32 @@ export type ItemDto = {
     effect: string;
     characterId: number;
 }
-type Progress = {
+export type Progress = {
+    id: number;
     book: string;
     section: number;
+    characterId: number;
+}
+
+export type ProgressDto = {
+    book: string;
+    section: number;
+    characterId: number;
 }
 
 type CharacterStore = {
     character: Character;
     setCharacter: (character: Character) => void;
-    progress?: Progress;
-    setProgress?: (progress: Progress) => void;
+    progress: Progress[];
+    setProgress: (progress: Progress[]) => void;
     allCharacters: Character[];
     setAllCharacters: (allCharacters: Character[]) => void;
     potions: Item[];
     setPotions: (potions: Item[]) => void;
     equipment: Item[];
     setEquipment: (equipment: Item[]) => void;
+    currentBook: Progress;
+    setCurrentBook: (currentBook: Progress) => void;
 }
 
 export const useCharacterStore = create<CharacterStore>((set) => ({
@@ -55,12 +65,14 @@ export const useCharacterStore = create<CharacterStore>((set) => ({
         gold: 0,
     },
     setCharacter: (character: Character) => set({ character }),
-    progress: undefined,
-    setProgress: (progress: Progress) => set({ progress }),
+    progress: [],
+    setProgress: (progress: Progress[]) => set({ progress }),
     allCharacters: [],
     setAllCharacters: (allCharacters:  Character[]) => set({allCharacters}),
     potions: [],
     setPotions: (potions: Item[]) => set({potions}),
     equipment: [],
-    setEquipment: (equipment: Item[]) => set({equipment})
+    setEquipment: (equipment: Item[]) => set({equipment}),
+    currentBook: {id: 0, book: "", section: 0, characterId: 0},
+    setCurrentBook: (currentBook: Progress) => set({currentBook})
 }))
