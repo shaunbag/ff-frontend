1ceb355ec986b46bbf6b7f495a05d7b7721ef5cc
ShaunMcelhinney <shaunbag@hotmail.com>
2026-02-16 10:18:17 +0000

    Types and api methods

--- src/store.ts
+++ src/store.ts
@@ -1,46 +1,6 @@
 import { create } from 'zustand';
+import type { Character, Progress, Item } from './utils/Types';
 
-export type Character = {
-    id: number;
-    name: string;
-    skill: number;
-    stamina: number;
-    luck: number;
-    gold: number;
-    provisions: number;
-}
-export type CharacterDto = {
-    name: string;
-    skill: number;
-    stamina: number;
-    luck: number;
-    gold: number;
-    provisions: number;
-}
-export type Item = {
-    id: number;
-    name: string;
-    effect: string;
-    characterId: number;
-}
-
-export type ItemDto = {
-    name: string;
-    effect: string;
-    characterId: number;
-}
-export type Progress = {
-    id: number;
-    book: string;
-    section: number;
-    characterId: number;
-}
-
-export type ProgressDto = {
-    book: string;
-    section: number;
-    characterId: number;
-}
 
 type CharacterStore = {
     character: Character;
